9e6a41cde6b665d6e76a18b4698dccba65c7663a
Peter Jang <peter.x.jang@gmail.com>
2018-08-02 18:38:57 -0500

    Use v-on:click to add and delete tasks

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="home">
+    <h1>Add a task</h1>
+    <input v-model="newTask.text">
+    <button v-on:click="addTask()">Add task</button>
+
     <h1>Tasks</h1>
     <ul>
-      <li v-for="task in tasks">
+      <li v-for="task in tasks" v-on:click="completeTask(task)">
         {{ task.text }}
       </li>
     </ul>
@@ -20,11 +24,23 @@ export default {
         { id: 1, text: "Take out the garbage", completed: false },
         { id: 2, text: "Make the bed", completed: false },
         { id: 3, text: "Mow the lawn", completed: false }
-      ]
+      ],
+      newTask: { text: "", completed: false }
     };
   },
   created: function() {},
-  methods: {},
+  methods: {
+    addTask: function() {
+      if (this.newTask.text) {
+        this.tasks.push(this.newTask);
+        this.newTask = { text: "", completed: false };
+      }
+    },
+    completeTask: function(inputTask) {
+      var index = this.tasks.indexOf(inputTask);
+      this.tasks.splice(index, 1);
+    }
+  },
   computed: {}
 };
 </script>
